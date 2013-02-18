3cff25172c529acf458485d5ad85414523890b40
Guchetl Murat <gmurka@gmail.com>
2013-02-18 03:45:40 +0300

    models: prev_next - now check and process null values and try to check fields nullability; behaviour is: value is None: gt - isnull False, lt - ignore, value is not None: gt - just gt, lt - (lg | isnull)

--- sakkada/models/prev_next/__init__.py
+++ sakkada/models/prev_next/__init__.py
@@ -23,31 +23,56 @@ class PrevNextModel(object):
 
         return ordering
 
-    def _get_next_or_previous_by_order(self, is_next=True, order_by=[], queryset=False, cachename=False, force=False, as_queryset=False):
+    def _get_next_or_previous_by_order(self, is_next=True, order_by=[],
+                                              queryset=False, cachename=False,
+                                               force=False, as_queryset=False):
         """Get next or previous element according current order_by settings"""
-        cachename = "__%s%s_in_nextprev_order_cache" % ('next' if is_next else 'prev', '_' + cachename if cachename else '')
+        cachename = "__%s%s_in_nextprev_order_cache" % ('next' if is_next else 'prev',
+                                                        '_' + cachename if cachename else '')
         if force or not hasattr(self, cachename):
-            queryset    = queryset or self._default_manager.get_query_set()
+            # get queryset and current ordering data
+            queryset    = queryset if isinstance(queryset, models.query.QuerySet) \
+                                   else self._default_manager.get_query_set()
             queryset    = queryset.order_by(*order_by) if len(order_by) else queryset
             ordering    = self._get_current_ordering(queryset)
-            order       = {'name':[i.lstrip('-') for i in ordering],
-                           'dir':[not i.startswith('-') if is_next else i.startswith('-') for i in ordering]}
-            ordering    = ordering if is_next else [i.lstrip('-') if i[0]=='-' else '-%s' % i for i in ordering]
-            filter = models.Q()
+            order       = {'name': [i.lstrip('-') for i in ordering],
+                           'dir': [not i.startswith('-') if is_next else i.startswith('-') \
+                                   for i in ordering]}
+            ordering    = ordering if is_next else [i.lstrip('-') if i[0]=='-' else '-%s' % i \
+                                                    for i in ordering]
+
+            # generate filter list
+            filter = []
             for name in order['name']:
                 index   = order['name'].index(name)
+                direc   = order['dir'][index]
                 value   = getattr(self, name)
                 equals  = dict([(i, getattr(self, i)) for i in order['name'][0:index]])
-                # null values exclude by "is not null", not by "gt" or "lt"
+
+                # null values specific processing: if direction is reverse - ignore
                 if value is None:
+                    # ignore for not loop by null values
+                    if not direc: continue
                     key, value = '%s__isnull' % name, False
+                    cmps = models.Q(**{key: value,})
                 else:
-                    key = '%s__%s' % (name, order['dir'][index] and 'gt' or 'lt')
-                equals[key] = value
-                filter = models.Q(**equals) | filter
+                    key = '%s__%s' % (name, direc and 'gt' or 'lt')
+                    cmps = models.Q(**{key: value,})
+
+                    # add isnull filter for reverse direction on nullable fields
+                    isnull = self.__class__._meta.get_field_by_name(name)[0].null
+                    if not direc and isnull:
+                        cmps = cmps | models.Q(**{'%s__isnull' % name: True,})
+
+                filter.append((models.Q(**equals) & cmps))
+
+            # get filter and exclude self.pk for strict filtering
+            filter = reduce(lambda x, y: y | x, filter) & ~models.Q(pk=self.pk)
+
             # return as queryset, filter and ordering params if True
             if as_queryset:
                 return queryset.filter(filter).order_by(*ordering), filter, ordering
+
             queryset = queryset.filter(filter).order_by(*ordering)[0:1]
             queryset = list(queryset) # preventing count call
             queryset = queryset[0] if len(queryset) else None
