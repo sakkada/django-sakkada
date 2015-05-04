ec6d04b7dc9396c6a9f041b08570a5bb18bfbd61
Guchetl Murat <gmurka@gmail.com>
2015-05-04 13:56:08 +0300

    admin:cache_clear: fix get_cache issue

--- sakkada/admin/views/cache_clear/__init__.py
+++ sakkada/admin/views/cache_clear/__init__.py
@@ -1,10 +1,11 @@
 from django.contrib import messages
 from django.utils.translation import ugettext_lazy as _
 from django.shortcuts import render, redirect
-from django.core.cache import cache
+from django.core.cache import get_cache
 
 def cache_clear_view(request, template_name=None, extra_context=None):
     """Clear system cache data."""
+    cache = get_cache('default')
     if request.POST.get('post', None):
         cache.clear()
         messages.success(request, _('Cache data has been successfully cleared.'))
